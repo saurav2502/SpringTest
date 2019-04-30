089750684532aaff1c51ba24f6e2a05b7065eb7f
Saurav Kumar <kumar_sauravsk@outlook.com>
2019-04-30 22:44:52 +0530

    new commit

--- src/main/java/com/spring/app/SpringTestApplication.java
+++ src/main/java/com/spring/app/SpringTestApplication.java
@@ -1,9 +1,11 @@
 package com.spring.app;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.spring.app.**.dao")
 public class SpringTestApplication {
 
 	public static void main(String[] args) {
